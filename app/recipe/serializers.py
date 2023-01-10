b88d862d0dbaec5d12e72ed0d4c755c323c0f6c7
Dhughes666 <graziemolto6@gmail.com>
2023-01-10 15:43:15 +0100

    Adding images to the recipes...

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -96,5 +96,15 @@ class RecipeDetailSerializer(RecipeSerializer):
         fields = RecipeSerializer.Meta.fields + ['description']
         
 
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipe."""
+    
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
+        
+
 
         
