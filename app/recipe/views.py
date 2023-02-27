ae05a1c78af1e96548457b95f4f1df9fa511f2cd
Dhughes666 <graziemolto6@gmail.com>
2023-02-27 10:48:55 +0100

    February commit

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -28,7 +28,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        return self.queryset.filter(
+            user=self.request.user).order_by('-id')
     
     def get_serializer_class(self):
         """Return the serializer class for request."""
@@ -42,6 +43,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+        
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data, status=status.HTTP_200_OK)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
         
 
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
